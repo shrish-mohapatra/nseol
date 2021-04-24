3d3fbb66643cad5a6a194a6a4bb91cbfb5022951
shrish-mohapatra <shrish.mohapatra@gmail.com>
2021-04-24 02:07:30 -0400

    start script

--- proto/js/index.js
+++ proto/js/index.js
@@ -1,108 +1,140 @@
-const MAIN_MOUSE_BUTTON = 0;
+'use strict';
 
-let mode = "pen"
+(function() {
 
-let sakura = new Sakura('body', {delay: 1000})
+  var sakura = new Sakura('body', {delay: 1000})
 
-let lb_pressure = document.getElementById("lb_pressure")
-let lb_btype = document.getElementById("lb_btype")
+  var socket = io();
+  var canvas = document.getElementById('myDraw');
+  var context = canvas.getContext('2d');
 
-let btnMode = document.getElementById("btnMode")
-let btnClear = document.getElementById("btnClear");
+  let lb_pressure = document.getElementById("lb_pressure")
+  let lb_btype = document.getElementById("lb_btype")
 
-let theCanvas = document.getElementById("myDraw");
-let theContext = prepareContext(theCanvas);
-let shouldDraw = false;
+  let btnMode = document.getElementById("btnMode")
+  let btnClear = document.getElementById("btnClear");
 
-theCanvas.addEventListener("pointerdown", start);
-theCanvas.addEventListener("pointerup", end);
-theCanvas.addEventListener("pointermove", move, false);
+  var current = {
+    color: '#1D1D1B'
+  };
 
-btnClear.addEventListener("click", event => {
-  clearCanvas(theContext);
-});
+  var drawing = false;
+  let inputMode = "pen"
 
-btnMode.addEventListener("click", (e) => {
-  let spMode = document.getElementById("spMode")
+  canvas.addEventListener('pointerdown', onMouseDown, false);
+  canvas.addEventListener('pointerup', onMouseUp, false);
+  canvas.addEventListener('pointerout', onMouseUp, false);
+  canvas.addEventListener('pointermove', throttle(onMouseMove, 10), false);
 
-  if(spMode.innerHTML == "touch_app") {
-    spMode.innerHTML =  "mode"
-    mode = "pen"
-  } else {
-    spMode.innerHTML = "touch_app"
-    mode = "touch"
+  btnClear.addEventListener("click", event => {
+    clearCanvas(context);
+    socket.emit('clearCanvas')
+  });
+
+  btnMode.addEventListener("click", (e) => {
+    let spMode = document.getElementById("spMode")
+  
+    if(spMode.innerHTML == "touch_app") {
+      spMode.innerHTML =  "mode"
+      inputMode = "pen"
+    } else {
+      spMode.innerHTML = "touch_app"
+      inputMode = "touch"
+    }
+  })
+
+  socket.on('drawing', onDrawingEvent);
+  socket.on('clearCanvas', () => clearCanvas(context))
+
+  window.addEventListener('resize', onResize, false);
+  onResize();
+
+
+  function drawLine(x0, y0, x1, y1, color, emit){
+    context.beginPath();
+    context.moveTo(x0, y0);
+    context.lineTo(x1, y1);
+    context.strokeStyle = color;
+    context.lineWidth = 2;
+
+    context.stroke();
+    context.closePath();
+
+    if (!emit) { return; }
+    var w = canvas.width;
+    var h = canvas.height;
+
+    socket.emit('drawing', {
+      x0: x0 / w,
+      y0: y0 / h,
+      x1: x1 / w,
+      y1: y1 / h,
+      color: color
+    });
   }
-})
 
-// -- HELPERS ---------
-function prepareContext() {
-  let canvasElement = document.getElementById("myDraw")
-
-  let dpr = window.devicePixelRatio || 1;
-  let rect = canvasElement.getBoundingClientRect();
-  canvasElement.width = rect.width * dpr;
-  canvasElement.height = rect.height * dpr;
-
-  let context = canvasElement.getContext("2d");
-  context.scale(dpr, dpr);
-
-  return context;
-}
-
-function setLineProperties(context, draw=true) {
-  context.strokeStyle = "#1D1D1B";
-  // context.lineJoin = "round";
-
-  if(draw) {
-    context.lineWidth = 3;
-    context.globalCompositeOperation = 'source-over';
-  } else {
-    context.lineWidth = 30;
-    context.globalCompositeOperation = 'destination-out';
+  function checkInput(e) {
+    return e.pointerType != inputMode && e.pointerType != "mouse"
   }
 
-  context.lineJoin = "round";
-  context.lineCap = "round";
+  function onMouseDown(e){
+    if (checkInput(e)) return
+    lb_btype.innerHTML = `buttonType: ${e.button}`
 
-  return context;
-}
-
-function clearCanvas(context) {
-  context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-}
-
-function checkInput(event) {
-  return event.pointerType != mode && event.pointerType != "mouse"
-}
-
-// -- EVENT HANDLERS --------
-function start(event) {
-  if (checkInput(event)) return
-
-  lb_btype.innerHTML = `buttonType: ${event.button}`
-
-  shouldDraw = true;
-
-  setLineProperties(theContext, event.button == 0);
-
-  theContext.beginPath();
-
-  let elementRect = event.target.getBoundingClientRect();
-  theContext.moveTo(event.clientX - elementRect.left, event.clientY - elementRect.top);
-}
-
-function end(event) {
-  if (checkInput(event)) return
-  shouldDraw = false;
-}
-
-function move(event) {
-  if (checkInput(event)) return
-  if (shouldDraw) {
-    lb_pressure.innerHTML = `pressure ${event.pressure}`
-
-    let elementRect = event.target.getBoundingClientRect();
-    theContext.lineTo(event.clientX - elementRect.left, event.clientY - elementRect.top);
-    theContext.stroke()
+    drawing = true;
+    current.x = e.clientX||e.touches[0].clientX;
+    current.y = e.clientY||e.touches[0].clientY;
   }
-}
+
+  function onMouseUp(e){
+    if (checkInput(e)) return
+    if (!drawing) { return; }
+    drawing = false;
+    drawLine(current.x, current.y, e.clientX||e.touches[0].clientX, e.clientY||e.touches[0].clientY, current.color, true);
+  }
+
+  function onMouseMove(e){
+    if (checkInput(e)) return
+    if (!drawing) { return; }
+
+    lb_pressure.innerHTML = `pressure ${e.pressure}`
+
+    drawLine(current.x, current.y, e.clientX||e.touches[0].clientX, e.clientY||e.touches[0].clientY, current.color, true);
+    current.x = e.clientX||e.touches[0].clientX;
+    current.y = e.clientY||e.touches[0].clientY;
+  }
+
+  function onColorUpdate(e){
+    current.color = e.target.className.split(' ')[1];
+  }
+
+  // limit the number of events per second
+  function throttle(callback, delay) {
+    var previousCall = new Date().getTime();
+    return function() {
+      var time = new Date().getTime();
+
+      if ((time - previousCall) >= delay) {
+        previousCall = time;
+        callback.apply(null, arguments);
+      }
+    };
+  }
+
+  function onDrawingEvent(data){
+    var w = canvas.width;
+    var h = canvas.height;
+    drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
+  }
+
+  // make the canvas fill its parent
+  function onResize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  }
+
+  function clearCanvas(context) {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+  }
+
+})();
